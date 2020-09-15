15d3f77a77e6357fc6010194b09da8640f893291
nikolajew-a-a <nikolajew.a.a@yandex.ru>
2020-09-15 23:27:23 +0300

    1.5.  Добавлена разметка для средних и больших экранов

--- Topic 1 5/app/src/main/java/com/example/android/topic15/MainActivity.java
+++ Topic 1 5/app/src/main/java/com/example/android/topic15/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final String COUNT = "count";
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +18,36 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView1 = findViewById(R.id.text1);
         TextView textView2 = findViewById(R.id.text2);
+        TextView textView3 = findViewById(R.id.text3);
+        TextView textView4 = findViewById(R.id.text4);
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(COUNT);
+            count++;
+        } else {
+            count = 0;
+        }
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            Toast.makeText(this, "Портретная ориентация", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Портретная ориентация. \n Активи пересоздано " + count + " раз(а).",
+                    Toast.LENGTH_SHORT).show();
             textView1.setText("Text top");
             textView2.setText("Text bottom");
+            textView3.setText("Only for medium and medium screens");
+            textView4.setText("Only for big screen");
         } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "Альбомная ориентация", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Альбомная ориентация\n Активи пересоздано " + count + " раз(а).",
+                    Toast.LENGTH_SHORT).show();
             textView1.setText("Text left");
             textView2.setText("Text right");
+            textView3.setText("Only for medium and medium screens");
+            textView4.setText("Only for big screen");
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(COUNT, count);
     }
 }
